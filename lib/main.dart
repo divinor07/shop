e455c37824e125f75f443cebd153a51c107f71cd
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-15 23:15:30 -0300

    Creating product grid component

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/views/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,25 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Minha Loja',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Minha Loja'),
-      ),
-      body: Center(
-        child: Text('Vamos desenvolver uma loja?'),
-      ),
+      home: ProductOverviewScreen(),
     );
   }
 }
